c2cd2a59a05af9f098a0f6debc3a06ab2e041e83
puneet <puneet16638@gmail.com>
2020-04-01 13:10:20 +0530

    intial commit

---
+++ src/main/java/org/postman/CalendarSlotBookingservice/CalendarSlotBookingserviceApplication.java
@@ -0,0 +1,13 @@
+package org.postman.CalendarSlotBookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarSlotBookingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarSlotBookingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/postman/CalendarSlotBookingservice/CalendarSlotBookingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.postman.CalendarSlotBookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarSlotBookingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
